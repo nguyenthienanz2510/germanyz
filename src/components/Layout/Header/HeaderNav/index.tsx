46e8f3459d5052ac3017373f6151a2cdc19fe79b
nguyenthienan <nguyenthienanz2510@gmail.com>
2023-01-16 19:34:39 +0700

    +Add: link icon nav

--- src/components/Layout/Header/HeaderNav/index.tsx
+++ src/components/Layout/Header/HeaderNav/index.tsx
@@ -6,18 +6,22 @@ const HeaderNav = () => {
     {
       name: 'Home',
       path: '/',
+      icon: 'https://img.fabet.cc/static/assets/images/components/header/sport.svg'
     },
     {
       name: 'About us',
       path: '/about-us',
+      icon: 'https://img.fabet.cc/static/assets/images/components/header/event.svg'
     },
     {
       name: 'Blog',
       path: '/blog',
+      icon: 'https://img.fabet.cc/static/assets/images/components/header/game-bai.svg'
     },
     {
       name: 'Shop',
       path: '/shop',
+      icon: 'https://img.fabet.cc/static/assets/images/components/header/quick-game.svg'
     },
   ]
 
@@ -30,7 +34,7 @@ const HeaderNav = () => {
               <NavLink className='hover:bg-[linear-gradient(180deg,#0DABFF,#ffffff)] dark:hover:bg-[linear-gradient(180deg,#001f5c,#000b27)]' key={index} href={item.path}>
                 <div className="flex flex-col items-center">
                   <img
-                    src="https://img.fabet.cc/static/assets/images/components/header/sport.svg"
+                    src={item.icon}
                     alt={item.name}
                   />
                   <span>{item.name}</span>
