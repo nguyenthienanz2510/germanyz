1d31d6ace90b84f26d8b67d4f5248545b24113cb
NguyenThienAn <nguyenthienan@mobcec.com>
2023-01-17 17:01:38 +0700

    +Add: newPosts in Homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,25 +25,68 @@ const IndexPage: NextPage = ({ data }: any) => {
     <MainLayout title="Homepage">
       <div className="w-[320px] float-left">
         <div className="pr-6">
-          <SideBarItem/>
-          <SideBarItem/>
-          <SideBarItem/>
-          <SideBarItem/>
+          <SideBarItem />
+          <SideBarItem />
+          <SideBarItem />
+          <SideBarItem />
         </div>
       </div>
+
       <div className="ml-[320px] pl-7 border-l-4">
-        {data.newPosts.posts.edges.map((post: any) => {
-          return (
-            <div key={post.node.postId} className="mb-5">
-              <h2>{post.node.title}</h2>
-              <div
-                dangerouslySetInnerHTML={{
-                  __html: sanitize(post.node.content ?? 'Nothing to preview'),
-                }}
-              />
+        <div>
+          <h2 className='mb-5'>Related posts</h2>
+          <div className="grid grid-cols-12 gap-x-7 gap-y-3">
+            <div className="col-span-7 row-span-2 transition-all duration-400 bg-[#fafcfa] hover:shadow-md cursor-pointer dark:bg-color-bg-dark-secondary dark:hover:bg-color-bg-dark-secondary-active">
+              <div>
+                <div>
+                  <img
+                    src={
+                      data.newPosts.posts.edges[0].node.featuredImage.node
+                        .mediaItemUrl
+                    }
+                    alt={data.newPosts.posts.edges[0].node.title}
+                  />
+                </div>
+                <div className='py-2 px-4'>
+                  <h6 className='text-truncate-2'>{data.newPosts.posts.edges[0].node.title}</h6>  
+                  <p className='mt-2 text-truncate-6'>{data.newPosts.posts.edges[0].node.content}</p>
+                </div>
+              </div>
             </div>
-          )
-        })}
+            <div className="col-span-5 transition-all duration-400 bg-[#fafcfa] hover:shadow-md cursor-pointer dark:bg-color-bg-dark-secondary dark:hover:bg-color-bg-dark-secondary-active">
+              <div>
+                <div>
+                  <img
+                    src={
+                      data.newPosts.posts.edges[1].node.featuredImage.node
+                        .mediaItemUrl
+                    }
+                    alt={data.newPosts.posts.edges[1].node.title}
+                  />
+                </div>
+                <div className='py-2 px-4'>
+                  <h6 className='text-truncate-2'>{data.newPosts.posts.edges[1].node.title}</h6>  
+                </div>
+              </div>
+            </div>
+            <div className="col-span-5 transition-all duration-400 bg-[#fafcfa] hover:shadow-md cursor-pointer dark:bg-color-bg-dark-secondary dark:hover:bg-color-bg-dark-secondary-active">
+              <div>
+                <div>
+                  <img
+                    src={
+                      data.newPosts.posts.edges[2].node.featuredImage.node
+                        .mediaItemUrl
+                    }
+                    alt={data.newPosts.posts.edges[2].node.title}
+                  />
+                </div>
+                <div className='py-2 px-4'>
+                  <h6 className='text-truncate-2'>{data.newPosts.posts.edges[2].node.title}</h6>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <WelcomeNotification />
     </MainLayout>
