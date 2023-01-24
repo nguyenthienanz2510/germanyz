43529531174db037183427836d28efe095b629f7
nguyenthienan <nguyenthienanz2510@gmail.com>
2023-01-24 15:17:05 +0700

    +Add: facebook chat plugin

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,6 +22,10 @@ const IndexPage: React.FC<IndexPageProps> = ({
 }) => {
 
   if (typeof window !== "undefined") {
+    var chatbox = document.getElementById('fb-customer-chat');
+    chatbox?.setAttribute("page_id", "611526916238214");
+    chatbox?.setAttribute("attribution", "biz_inbox");
+
     window.fbAsyncInit = function() {
       FB.init({
         xfbml            : true,
@@ -52,32 +56,8 @@ const IndexPage: React.FC<IndexPageProps> = ({
       <WelcomeNotification />
 
     <div id="fb-root"></div>
-    <div id="fb-customer-chat" className="fb-customerchat">
+    <div id="fb-customer-chat" className="fb-customerchat testhihi">
     </div>
-
-    <script>
-      var chatbox = document.getElementById('fb-customer-chat');
-      chatbox.setAttribute("page_id", "611526916238214");
-      chatbox.setAttribute("attribution", "biz_inbox");
-    </script>
-
-    {/* <script>
-      window.fbAsyncInit = function() {
-        FB.init({
-          xfbml            : true,
-          version          : 'v15.0'
-        })
-      };
-
-      (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0]
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));
-
-    </script> */}
 
     </MainLayout>
   )
--- src/pages/page/preview/[id].tsx
+++ src/pages/page/preview/[id].tsx
@@ -1,56 +1,57 @@
-import { GetServerSideProps } from 'next'
-import MainLayout from '../../../components/Layout/MainLayout'
-import { GetPageByIdDocument } from '../../../generated/graphql'
-import client from '../../../lib/apolloClient'
-import { getAuthToken } from '../../../utils/cookies'
-import { sanitize } from '../../../utils/miscellaneous'
-import { getLoginPreviewRedirectUrl } from '../../../utils/redirects'
-import { handleRedirectsAndReturnData } from '../../../utils/slug'
+// import { GetServerSideProps } from 'next'
+// import MainLayout from '../../../components/Layout/MainLayout'
+// import { GetPageByIdDocument } from '../../../generated/graphql'
+// import client from '../../../lib/apolloClient'
+// import { getAuthToken } from '../../../utils/cookies'
+// import { sanitize } from '../../../utils/miscellaneous'
+// import { getLoginPreviewRedirectUrl } from '../../../utils/redirects'
+// import { handleRedirectsAndReturnData } from '../../../utils/slug'
 
-const PagePreview = ({ data }: any) => {
+const PagePreview = () => {
   return (
-    <MainLayout title="Post preview">
-      <div
-        dangerouslySetInnerHTML={{
-          __html: sanitize(data?.page?.content ?? 'Nothing to preview'),
-        }}
-      />
-    </MainLayout>
+    <div>Page not found</div>
+    // <MainLayout title="Post preview">
+    //   <div
+    //     dangerouslySetInnerHTML={{
+    //       __html: sanitize(data?.page?.content ?? 'Nothing to preview'),
+    //     }}
+    //   />
+    // </MainLayout>
   )
 }
 
 export default PagePreview
 
-export const getServerSideProps: GetServerSideProps = async context => {
-  const authToken = getAuthToken(context.req)
+// export const getServerSideProps: GetServerSideProps = async context => {
+//   const authToken = getAuthToken(context.req)
 
-  const { params } = context || {}
-  const { data, errors } = await client.query({
-    query: GetPageByIdDocument,
-    variables: {
-      id: Number(params?.id ?? ''),
-    },
-    context: {
-      headers: {
-        authorization: authToken ? `Bearer ${authToken}` : '',
-      },
-    },
-  })
+//   const { params } = context || {}
+//   const { data, errors } = await client.query({
+//     query: GetPageByIdDocument,
+//     variables: {
+//       id: Number(params?.id ?? ''),
+//     },
+//     context: {
+//       headers: {
+//         authorization: authToken ? `Bearer ${authToken}` : '',
+//       },
+//     },
+//   })
 
-  const defaultProps = {
-    props: {
-      data: data || {},
-    },
-  }
+//   const defaultProps = {
+//     props: {
+//       data: data || {},
+//     },
+//   }
 
-  const loginRedirectURL = getLoginPreviewRedirectUrl('page', params?.id ?? '')
+//   const loginRedirectURL = getLoginPreviewRedirectUrl('page', params?.id ?? '')
 
-  return handleRedirectsAndReturnData(
-    defaultProps,
-    data,
-    errors,
-    'page',
-    true,
-    loginRedirectURL,
-  )
-}
+//   return handleRedirectsAndReturnData(
+//     defaultProps,
+//     data,
+//     errors,
+//     'page',
+//     true,
+//     loginRedirectURL,
+//   )
+// }
