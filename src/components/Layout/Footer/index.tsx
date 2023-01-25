b313ff9498060b23de80e5bf7d80e035d6155d79
nguyenthienan <nguyenthienanz2510@gmail.com>
2023-01-25 10:25:56 +0700

    +Add: Facebook chat plugin

--- src/components/Layout/Footer/index.tsx
+++ src/components/Layout/Footer/index.tsx
@@ -1,9 +1,34 @@
 const Footer = () => {
-    return (
-        <div className="h-16 bg-color-bg-dark-header flex justify-center items-center">
-            Footer
-        </div>
-    );
+  if (typeof window !== 'undefined') {
+    var chatbox = document.getElementById('fb-customer-chat')
+    chatbox?.setAttribute('page_id', '611526916238214')
+    chatbox?.setAttribute('attribution', 'biz_inbox')
+
+    window.fbAsyncInit = function () {
+      FB.init({
+        xfbml: true,
+        version: 'v15.0',
+      })
+    }
+
+    ;(function (d, s, id) {
+      var js: any = d.getElementsByTagName(s)[0]
+      var fjs: any = d.getElementsByTagName(s)[0]
+      if (d.getElementById(id)) return
+      js = d.createElement(s)
+      js.id = id
+      js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js'
+      fjs?.parentNode?.insertBefore(js, fjs)
+    })(document, 'script', 'facebook-jssdk')
+  }
+
+  return (
+    <div className="h-16 dark:bg-color-bg-dark-header flex justify-center items-center">
+      Footer
+      <div id="fb-root"></div>
+      <div id="fb-customer-chat" className="fb-customerchat"></div>
+    </div>
+  )
 }
 
-export default Footer;
+export default Footer
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,30 +21,6 @@ const IndexPage: React.FC<IndexPageProps> = ({
   blogCategories,
 }) => {
 
-  if (typeof window !== "undefined") {
-    var chatbox = document.getElementById('fb-customer-chat');
-    chatbox?.setAttribute("page_id", "611526916238214");
-    chatbox?.setAttribute("attribution", "biz_inbox");
-
-    window.fbAsyncInit = function() {
-      FB.init({
-        xfbml            : true,
-        version          : 'v15.0'
-      })
-    };
-  
-    (function(d, s, id) {
-      var js:any = d.getElementsByTagName(s)[0]
-      var fjs:any = d.getElementsByTagName(s)[0]
-      if (d.getElementById(id)) return;
-      js = d.createElement(s); js.id = id;
-      js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-      fjs?.parentNode?.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-  }
-
-  
-  
   return (
     <MainLayout
       latestPosts={latestPosts}
@@ -54,10 +30,6 @@ const IndexPage: React.FC<IndexPageProps> = ({
       <LatestPostsContainer latestPosts={latestPosts} />
 
       <WelcomeNotification />
-
-    <div id="fb-root"></div>
-    <div id="fb-customer-chat" className="fb-customerchat testhihi">
-    </div>
 
     </MainLayout>
   )
