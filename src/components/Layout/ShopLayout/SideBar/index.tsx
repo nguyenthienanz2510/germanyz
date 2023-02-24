fc2f25144865a88c0b66a22dd4a0830d7a1a1710
NguyenThienAn <nguyenthienan@mobcec.com>
2023-02-24 11:54:16 +0700

    Fix: Layout Product Page

--- src/components/Layout/ShopLayout/SideBar/index.tsx
+++ src/components/Layout/ShopLayout/SideBar/index.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { GetBlogCategoriesQuery, GetPostsQuery } from '../../../../generated/graphql'
 import BlogCategories from '../../SideBar/BlogCategories'
 import LatestPosts from '../../SideBar/LatestPosts'
-import ProductCategories from '../../SideBar/ProductCategories'
 
 interface Props {
   blogCategories: GetBlogCategoriesQuery
@@ -13,7 +12,6 @@ const ShopLayoutSideBar: React.FC<Props> = ({blogCategories, latestPosts}) => {
   return (
     <div className="w-full mt-10 lg:mt-0 lg:w-[320px] lg:absolute lg:top-0 lg:left-0">
       <div className="lg:pr-6">
-        <ProductCategories blogCategories={blogCategories}/>
         <BlogCategories blogCategories={blogCategories}/>
         <LatestPosts latestPosts={latestPosts}/>
       </div>
