94e73f7c4be01e4beefc1594dd5644dd93379893
nguyenthienan <nguyenthienanz2510@gmail.com>
2023-03-04 15:22:30 +0700

    TEST MIDLEWARE

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,29 @@
 
-const middleware = () => {
-//   const { nextUrl, geo } = req;
-//   const country = geo?.country || 'Viet Nam';
-//   nextUrl.searchParams.set('country', country);
-//   return NextResponse.rewrite(nextUrl);
-};
+// const middleware = () => {
+// //   const { nextUrl, geo } = req;
+// //   const country = geo?.country || 'Viet Nam';
+// //   nextUrl.searchParams.set('country', country);
+// //   return NextResponse.rewrite(nextUrl);
+// };
 
-export default middleware;
+import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
+
+// export default middleware;
+
+export async function middleware(request: NextRequest, _next: NextFetchEvent) {
+    const res = NextResponse.next();
+    let ip = request.ip ?? request.headers.get('x-real-ip')
+    const forwardedFor = request.headers.get('x-forwarded-for')
+    if(!ip && forwardedFor){
+      ip = forwardedFor.split(',').at(0) ?? 'Unknown'
+    } 
+
+    console.log("MIDDLEWARE", ip)
+    if(ip){
+      res.cookies.set("user-ip", ip, {
+        httpOnly: false,
+      });
+    }
+    
+    return res;
+  }
